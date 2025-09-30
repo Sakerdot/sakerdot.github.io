cb0a23eaffed5591f738292ebae114ef6aa64fa4
Adrian Samatan <Sakerdot@users.noreply.github.com>
2025-09-30 20:41:47 +0100

    Update welcome message to Spanish

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Bienvenid@!</h1>
+<p>Todas las quejas <a href="https://diego.contact/">aquí</a></p>
